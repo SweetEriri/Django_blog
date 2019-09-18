d33a8339df144d00c190a9a9e118400c70770616
SweetEriri <SweetEriri@Gmail.com>
2019-09-19 00:35:23 +0800

    分页修复

--- SweetEriri_blog/SweetEriri_blog/settings.py
+++ SweetEriri_blog/SweetEriri_blog/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'blog',
     'comments',
+    'dj_pagination',
 ]
 
 MIDDLEWARE = [
@@ -49,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'dj_pagination.middleware.PaginationMiddleware',
 ]
 
 ROOT_URLCONF = 'SweetEriri_blog.urls'
--- SweetEriri_blog/blog/views.py
+++ SweetEriri_blog/blog/views.py
@@ -4,6 +4,8 @@ from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView, DetailView
 from comments.forms import CommentForm
 from .models import Post, Category
+from django.core.paginator import Paginator
+from django.shortcuts import render
 
 def index(request):
     post_list = Post.objects.all()
@@ -14,62 +16,15 @@ class IndexView(ListView):
     model = Post
     template_name = 'blog/index.html'
     context_object_name = 'post_list'
-    paginate_by = 5
+    paginate_by = 2
 
-def get_context_data(self, **kwargs):
-    context = super().get_context_data(**kwargs)
-    paginator = context.get('paginator')
-    page = context.get('page_obj')
-    is_paginated = context.get('is_paginated')
-    pagination_data = self.pagination_data(paginator, page, is_paginated)
-    context.update(pagination_data)
-    return context
+def listing(request):
+    contact_list = Contacts.obhects.all()  # 得到一个QuerySet
+    page = request.GET.get('page', 1)  # 得到访问的是第几页，默认值为1
+    contacts = paginator.get_page(page)  # 获取对应页获取的内容
+    return render(request, 'blog/list.html', {'contacts': contacts})
 
-def pagination_data(self, paginator, page, is_paginated):
-        if not is_paginated:
-            return {}
-        left = []
-        right = []
-        left_has_more = False
-        right_has_more = False
-        first = False
-        last = False
-        page_number = page.number
-        total_pages = paginator.num_pages
-        page_range = paginator.page_range
-        if page_number == 1:
-            right = page_range[page_number:page_number + 2]
-        if right[-1] < total_pages - 1:
-            right_has_more = True
-        if right[-1] < total_pages:
-            last = True
-        elif page_number == total_pages:
-            left = page_range[(page_number - 3) if (page_number - 3) > 0 else 0:page_number - 1]
-        if left[0] > 2:
-            left_has_more = True
-        if left[0] > 1:
-            first = True
-        else:
-            left = page_range[(page_number - 3) if (page_number - 3) > 0 else 0:page_number - 1]
-            right = page_range[page_number:page_number + 2]
-            if right[-1] < total_pages - 1:
-                right_has_more = True
-            if right[-1] < total_pages:
-                last = True
-            if left[0] > 2:
-                left_has_more = True
-            if left[0] > 1:
-                first = True
-                data = {
-                    'left': left,
-                    'right': right,
-                    'left_has_more': left_has_more,
-                    'right_has_more': right_has_more,
-                    'first': first,
-                    'last': last,
-                }
 
-                return data
 
 def detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
